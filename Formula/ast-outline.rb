67af239b1c5cf1b55f8553669af568701e1aa6b7
Aero <aero.windwalker@gmail.com>
2026-04-27 23:02:03 +0800

    chore: update brew formula

--- Formula/ast-outline.rb
+++ Formula/ast-outline.rb
@@ -1,8 +1,8 @@
 class AstOutline < Formula
   desc "Fast, AST-based structural outline for source files"
   homepage "https://github.com/aeroxy/ast-outline"
-  url "https://github.com/aeroxy/ast-outline/releases/download/0.1.1/ast-outline-macos-arm64.zip"
-  sha256 "c76da0aae16486843bb55c2e553a1d59f4a52aa43fa4b6199a72c99bfea51683"
+  url "https://github.com/aeroxy/ast-outline/releases/download/0.1.2/ast-outline-macos-arm64.zip"
+  sha256 "3ee17d16acaa8363e24d2bd5d4aa3298ea5b84d6e3d8147645b2bd769447f429"
   license "MIT"
 
   def install
